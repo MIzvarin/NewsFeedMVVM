69669f8b14d204b12e166b7bdb9ab5fd065516ee
MIzvarin <86963802+MIzvarin@users.noreply.github.com>
2022-02-07 12:24:43 +0300

    Initial Commit

---
+++ NewsFeedMVVM/NewsFeedMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsFeedMVVMApp.swift
+//  NewsFeedMVVM
+//
+//  Created by Максим Изварин on 07.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct NewsFeedMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
